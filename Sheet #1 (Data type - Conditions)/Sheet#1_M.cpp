820aa9d23aaf1860ff298052adaefca0bb5e0b0f
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-09-18 15:06:57 +0530

    Update Sheet#1_M.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_M.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_M.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    char ch;
+    cin>>ch;
+    
+    if(ch >='0' && ch <= '9'){
+        cout<<"IS DIGIT";
+    }
+    else{
+        cout<<"ALPHA"<<endl;
+        if(ch >='A' && ch <= 'Z')
+            cout<<"IS CAPITAL";
+        else
+            cout<<"IS SMALL";
+    }
+}
