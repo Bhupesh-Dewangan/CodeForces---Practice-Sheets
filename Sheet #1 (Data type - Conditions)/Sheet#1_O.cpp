f7e192b2cf34031e00c0b849be861735dde16982
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-09-25 22:53:09 +0530

    Update Sheet#1_O.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_O.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_O.cpp
@@ -1,1 +1,39 @@
-Sheet#1_N.cpp
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    
+    string s;
+    cin>>s;
+    int i=0;
+    long long a = 0,b = 0;
+    while( s[i] != '\0'){
+        if(s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*')
+            break;
+        else
+            a  = a*10 + (s[i]-'0');
+            
+        i++;
+    }
+    
+    int op = i;
+    i++;
+    
+    while( s[i] != '\0'){
+        b  = b*10 + (s[i]-'0');
+        i++;
+    }
+    
+    switch(s[op]){
+        case '+' : cout<<a+b;
+        break;
+        case '-': cout<<a-b;
+        break;
+        case '*' : cout<<a*b;
+        break;
+        case '/': cout<<a/b;
+        break;
+    }
+    
+    
+}
