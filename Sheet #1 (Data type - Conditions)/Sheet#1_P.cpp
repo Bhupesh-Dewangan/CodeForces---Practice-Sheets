cbd319e6c24da8a380bd058729b5e28378283457
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-09-27 22:48:58 +0530

    Update Sheet#1_P.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_P.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_P.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    
+    int rem;
+    while(n>0){
+        rem = n%10;
+        n /= 10;
+    }
+    
+    if(rem%2 == 0) cout<<"EVEN";
+    else cout<<"ODD";
+}
