4d441020b815908b30bbc67e299287a211b94c5b
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-09-29 19:45:31 +0530

    Update Sheet#1_N.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_N.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_N.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    char ch;
+    cin>>ch;
+    
+    if(ch >= 'A' && ch <= 'Z'){
+        cout<<char(ch + 32);
+    }
+    else{
+        cout<<char(ch - 32);
+    }
+}
