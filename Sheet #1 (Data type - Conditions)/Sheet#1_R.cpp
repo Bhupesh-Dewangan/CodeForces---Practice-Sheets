dc253e0126b741fab5e448be5c1a5190570de2c0
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-09-29 19:46:00 +0530

    Update Sheet#1_R.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_R.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_R.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    long long x;
+    cin>>x;
+    
+    int year = x/365;
+    x %= 365;
+    int month = x/30;
+    x %= 30;
+    int day = x;
+    
+    cout<<year<<" years"<<endl;
+    cout<<month<<" months"<<endl;
+    cout<<day<<" days";
+    
+}
