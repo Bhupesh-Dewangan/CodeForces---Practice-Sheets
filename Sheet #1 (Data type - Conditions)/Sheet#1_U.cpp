3f9b4bbcd179af27c36d2ad688f1ca1148b455aa
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-09-30 20:58:38 +0530

    Update Sheet#1_U.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_U.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_U.cpp
@@ -1,1 +1,18 @@
-
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    double num;
+    cin>>num;
+    
+    int floorVal = floor(num);
+    int ceilVal = ceil(num);
+    
+    if(floorVal == ceilVal)
+        cout<<"int "<<floorVal;
+    else
+        cout<<"float "<<floorVal<<" "<<(num - floorVal);
+ 
+    return 0;
+}
