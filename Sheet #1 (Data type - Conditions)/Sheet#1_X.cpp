8dca98b8ba902b48ba908aa35395171d5498b7ec
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-02 17:57:49 +0530

    Update Sheet#1_X.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_X.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_X.cpp
@@ -1,1 +1,20 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long l1,l2,l3,l4;
+    cin>>l1>>l2>>l3>>l4;
+    
+    if(l1 <= l3){
+        if( l3 <= l2)
+            cout<<l3<<" "<<min(l2,l4);
+        else
+            cout<<-1;
+    }
+    else{
+        if( l1 <= l4)
+            cout<<l1<<" "<<min(l2,l4);
+        else
+            cout<<-1;
+    }
+}
