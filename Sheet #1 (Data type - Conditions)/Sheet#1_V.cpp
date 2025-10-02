7270332d0bdfc44cb483608bb61b66c71bbe04e7
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-02 17:56:25 +0530

    Update Sheet#1_V.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_V.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_V.cpp
@@ -1,1 +1,22 @@
-Sheet#1_U.cpp
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int A, B;
+    char S;
+ 
+    // Read input: A, S, B
+    cin >> A >> S >> B;
+ 
+    // Check the comparison and print result
+    if (S == '<' && A < B)
+        cout << "Right" << endl;
+    else if (S == '>' && A > B)
+        cout << "Right" << endl;
+    else if (S == '=' && A == B)
+        cout << "Right" << endl;
+    else
+        cout << "Wrong" << endl;
+ 
+    return 0;
+}
