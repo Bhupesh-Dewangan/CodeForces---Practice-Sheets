5c19e793dec7ba1a6018d5b3e8e46655586a61e9
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-02 17:57:10 +0530

    Update Sheet#1_W.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_W.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_W.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int A, B, C;
+    char op, eq;
+ 
+    // Read input in the format: A op B = C
+    cin >> A >> op >> B >> eq >> C;
+ 
+    int result;
+ 
+    // Perform the operation based on op
+    if (op == '+') {
+        result = A + B;
+    } else if (op == '-') {
+        result = A - B;
+    } else if (op == '*') {
+        result = A * B;
+    }
+ 
+    // Output result
+    if (result == C) {
+        cout << "Yes" << endl;
+    } else {
+        cout << result << endl;
+    }
+ 
+    return 0;
+}
 
