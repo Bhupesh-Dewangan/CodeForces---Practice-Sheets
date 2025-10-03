b5aea1d456133593aa1eee3bf82fc83f7d874029
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-03 14:08:53 +0530

    Update Sheet#1_Z.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_Z.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_Z.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <cmath>  // For log
+using namespace std;
+ 
+int main() {
+    double A, B, C, D;
+    cin >> A >> B >> C >> D;
+ 
+    // Use logarithm to compare powers
+    double left = B * log(A);
+    double right = D * log(C);
+ 
+    if (left > right)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+ 
+    return 0;
+}
