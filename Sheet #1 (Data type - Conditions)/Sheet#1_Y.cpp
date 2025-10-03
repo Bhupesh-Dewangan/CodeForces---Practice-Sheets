b55e5a1e34592b21e8f8b9f920b103561c4e59f6
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-03 14:08:14 +0530

    Update Sheet#1_Y.cpp

--- Sheet #1 (Data type - Conditions)/Sheet#1_Y.cpp
+++ Sheet #1 (Data type - Conditions)/Sheet#1_Y.cpp
@@ -1,1 +1,16 @@
-Sheet#1_X.cpp
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    
+    a = a % 100;
+    b = b % 100;
+    c = c % 100;
+    d = d % 100;
+    
+    int ans = (a*b*c*d)%100;
+    if( ans >= 0 && ans <= 9) cout<<"0";
+    cout<<ans;
+}
