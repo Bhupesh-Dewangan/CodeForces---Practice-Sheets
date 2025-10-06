d72f3fbe3547f9b94599884c43d655aa42cdcc3d
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-06 22:03:01 +0530

    Update Contest#1_A.cpp

--- Contest #1/Contest#1_A.cpp
+++ Contest #1/Contest#1_A.cpp
@@ -1,1 +1,10 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    double d, dp;
+    cin>>d>>dp;
+    
+    double og = dp / (1 - (d/100));
+    cout<<fixed<<setprecision(2)<<og;
+}
