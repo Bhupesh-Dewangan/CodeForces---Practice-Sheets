ba24f14da637eeb539857a89bce499f345d10c3b
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-12 23:12:45 +0530

    Update Contest#1_F.cpp

--- Contest #1/Contest#1_F.cpp
+++ Contest #1/Contest#1_F.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream> 
+using namespace std;
+ 
+int main(){
+    long long a,b;
+    cin>>a>>b;
+    long long ans=0;
+    int base = 1;
+    while( a != 0 || b != 0){
+        int sum = (a&1) + (b&1);
+        if( sum < 2 ) 
+            ans += sum*base;
+        
+        base = base*2;
+        a = a>>1;
+        b = b>>1;
+    }
+    
+    cout<<ans;
+}
