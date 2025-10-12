db0d35abdd559135106f3806f50ca36c2a0941d7
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-12 23:12:16 +0530

    Update Contest#1_E.cpp

--- Contest #1/Contest#1_E.cpp
+++ Contest #1/Contest#1_E.cpp
@@ -1,1 +1,10 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    short int a,b;
+    cin>>a>>b;
+    
+    if( abs(a-b) == 1 || ((a == b) && a != 0)) cout<<"YES";
+    else cout<<"NO";
+}
