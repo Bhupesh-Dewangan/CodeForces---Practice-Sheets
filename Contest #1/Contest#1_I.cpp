996e634a127b2fe5b186e81ac7ffe3a67e851675
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-13 17:01:01 +0530

    Update Contest#1_I.cpp

--- Contest #1/Contest#1_I.cpp
+++ Contest #1/Contest#1_I.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    int unit = (n%10);
+    n = n/10;
+    int tenth = (n%10);
+    if( unit%tenth == 0 || tenth%unit == 0 )
+        cout<<"YES";
+    else cout<<"NO";
+}
 
