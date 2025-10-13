5b2abc167014347a76ae721053d18cd6024c9b99
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-13 17:00:39 +0530

    Update Contest#1_H.cpp

--- Contest #1/Contest#1_H.cpp
+++ Contest #1/Contest#1_H.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    long long n, k, a;
+    cin >> n >> k >> a;
+ 
+    long long mul = n * k;  // safe in long long
+    if (mul % a == 0) { // perfectly divisible → integer
+        long long result = mul / a;
+        if (result >= INT_MIN && result <= INT_MAX)
+            cout << "int\n";
+        else
+            cout << "long long\n";
+    } else {
+        cout << "double\n";
+    }
+ 
+    return 0;
+}
