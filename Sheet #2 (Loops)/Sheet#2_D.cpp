a381da4ba65052d32968ee1bd5913e675e628ef1
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-18 17:46:37 +0530

    Update Sheet#2_D.cpp

--- Sheet #2 (Loops)/Sheet#2_D.cpp
+++ Sheet #2 (Loops)/Sheet#2_D.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int num;
+    while(cin>>num){
+        if(num == 1999){
+            cout<<"Correct";
+            break;
+        }
+        else cout<<"Wrong";
+        
+        cout<<endl;
+    }
+    
+}
