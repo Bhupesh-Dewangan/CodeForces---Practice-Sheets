31005526a4e4073b405e636d0827e7c12cd5d294
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-18 17:47:03 +0530

    Implement maximum number finder in Sheet#2_E.cpp
    Add a program to find the maximum number from user input.

--- Sheet #2 (Loops)/Sheet#2_E.cpp
+++ Sheet #2 (Loops)/Sheet#2_E.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    int max = 0;
+    for(int i=0; i<n; i++){
+        int temp;
+        cin>>temp;
+        
+        if(temp > max) max = temp;
+    }
+    
+    cout<<max;
+}
