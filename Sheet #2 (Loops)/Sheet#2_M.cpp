7a65f50dcda38af8c52f26dfbd6039243d0a1b3d
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-24 23:21:40 +0530

    Implement lucky number finder in Sheet#2_M.cpp

--- Sheet #2 (Loops)/Sheet#2_M.cpp
+++ Sheet #2 (Loops)/Sheet#2_M.cpp
@@ -1,1 +1,32 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+bool isLucky(int num) {
+    string s = to_string(num);
+    for (char c : s) {
+        if (c != '4' && c != '7')
+            return false;
+    }
+    return true;
+}
+ 
+int main() {
+    int A, B;
+    cin >> A >> B;
+    vector<int> luckyNumbers;
+ 
+    for (int i = A; i <= B; i++) {
+        if (isLucky(i)) {
+            luckyNumbers.push_back(i);
+        }
+    }
+ 
+    if (luckyNumbers.empty()) {
+        cout << -1;
+    } else {
+        for (int num : luckyNumbers) {
+            cout << num << " ";
+        }
+    }
+    return 0;
+}
