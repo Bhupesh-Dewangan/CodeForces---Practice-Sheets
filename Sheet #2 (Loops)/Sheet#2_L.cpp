1a1e425dc3f957c126209d47f6e9ae9823a5cd8b
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-24 23:21:05 +0530

    Add GCD calculation using built-in function

--- Sheet #2 (Loops)/Sheet#2_L.cpp
+++ Sheet #2 (Loops)/Sheet#2_L.cpp
@@ -1,1 +1,10 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int A, B;
+    cin >> A >> B;
+    // Using built-in gcd function (C++17)
+    cout << __gcd(A, B);
+    return 0;
+}
