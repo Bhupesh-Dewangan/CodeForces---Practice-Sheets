8696ebd68c943c88c935850f6b0cedf515959746
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-24 23:19:38 +0530

    Implement number reversal and palindrome check
    Add a program to reverse a number and check for palindrome.

--- Sheet #2 (Loops)/Sheet#2_I.cpp
+++ Sheet #2 (Loops)/Sheet#2_I.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    long long num;
+    cin >> num;
+    
+    long long original = num;
+    long long reversed = 0;
+    
+    while (num > 0) {
+        int digit = num % 10;           
+        reversed = reversed * 10 + digit; 
+        num /= 10;                      
+    }
+    
+    cout<<reversed<<endl;
+    if(reversed == original) cout<<"YES";
+    else cout<<"NO";
+}
