5e846c3f8fe3a0461728163047b7ea0ad77bf431
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-25 17:01:06 +0530

    Update Sheet#2_N.cpp

--- Sheet #2 (Loops)/Sheet#2_N.cpp
+++ Sheet #2 (Loops)/Sheet#2_N.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    char s;
+    cin>>s;
+    int n;
+    cin>>n;
+    
+    int temp;
+    for(int i=0; i<n; i++){
+        cin>>temp;
+        
+        for(int i=0; i<temp; i++)
+            cout<<s;
+            
+        cout<<endl;
+    }
+    
+    
+}
