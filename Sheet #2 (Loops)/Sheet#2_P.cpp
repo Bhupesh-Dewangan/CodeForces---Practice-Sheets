7578b77da3e71c0d49be0b8bb4553a314059895a
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-25 17:02:08 +0530

    Update Sheet#2_P.cpp

--- Sheet #2 (Loops)/Sheet#2_P.cpp
+++ Sheet #2 (Loops)/Sheet#2_P.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int N;
+    cin >> N;
+ 
+    for (int i = N; i >= 1; i--) {       // Start from N down to 1
+        for (int j = 1; j <= i; j++) {   // Print i stars in each row
+            cout << '*';
+        }
+        cout << '\n';
+    }
+ 
+    return 0;
+}
