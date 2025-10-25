1794107a92f3c5308b5718aeacd590c7d6fa5e44
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-25 17:02:35 +0530

    Update Sheet#2_Q.cpp

--- Sheet #2 (Loops)/Sheet#2_Q.cpp
+++ Sheet #2 (Loops)/Sheet#2_Q.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    long long t;
+    cin >> t;
+ 
+    while (t--) {
+        long long n;
+        cin >> n;
+ 
+        if (n == 0) {
+            cout << 0;
+        } else {
+            bool first = true;
+            while (n != 0) {
+                if (!first) cout << " ";
+                cout << n % 10;
+                n /= 10;
+                first = false;
+            }
+        }
+        cout << endl;
+    }
+}
 
