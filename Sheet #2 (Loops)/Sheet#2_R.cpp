970cf4487d652ee40f32b4008419be5ec602bfba
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-26 20:28:44 +0530

    Implement loop to sum and display range of numbers
    Added a loop to print numbers between n and m and calculate their sum.

--- Sheet #2 (Loops)/Sheet#2_R.cpp
+++ Sheet #2 (Loops)/Sheet#2_R.cpp
@@ -1,1 +1,19 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n,m;
+    while(cin>>n>>m){
+        if(n <= 0 || m <= 0) return 0;
+        
+        int mi = min(n,m);
+        int ma = max(n,m);
+        int sum = 0;
+        for(int i=mi; i<=ma; i++){
+            cout<< i << " ";
+            sum += i;
+        }
+        
+        cout<<"sum ="<<sum<<endl;
+    }
+}
