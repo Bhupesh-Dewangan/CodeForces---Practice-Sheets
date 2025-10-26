b838bd80f18dece148b7cdba06b9dbc6079ab29c
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-26 20:29:20 +0530

    Add sum calculation for odd numbers in C++
    Implement a program to calculate the sum of odd numbers between two inputs.

--- Sheet #2 (Loops)/Sheet#2_S.cpp
+++ Sheet #2 (Loops)/Sheet#2_S.cpp
@@ -1,1 +1,28 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    short int t;
+    cin>>t;
+    
+    while(t--){
+        int x,y,a,b;
+        cin>>a>>b;
+        
+        x = min(a,b);
+        y = max(a,b);
+        long long sum=0;
+        int start;
+        if(x%2==0) start = x+1;
+        else start = x+2;
+        
+        for(int i= start; i<y; i++){
+            sum += i;
+            i++;
+        }
+        
+        cout<<sum<<endl;
+        
+        
+    }
+}
