0b0e034c22654976bb50d9267b2bb9d88fb05ab0
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-10-26 20:29:47 +0530

    Add pyramid star pattern program
    This program takes an integer input N and prints a pyramid pattern of stars.

--- Sheet #2 (Loops)/Sheet#2_T.cpp
+++ Sheet #2 (Loops)/Sheet#2_T.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int N;
+    cin >> N;
+ 
+    for (int i = 1; i <= N; i++) {
+        // Print spaces
+        for (int space = 1; space <= N - i; space++) {
+            cout << " ";
+        }
+        // Print stars
+        for (int star = 1; star <= (2 * i - 1); star++) {
+            cout << "*";
+        }
+        cout << "\n"; // Move to the next row
+    }
+ 
+    return 0;
+}
