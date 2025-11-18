9c068b7b868fff6b3c4ba5874ef615c343ba9849
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-18 23:36:12 +0530

    Update Sheet#2_U.cpp

--- Sheet #2 (Loops)/Sheet#2_U.cpp
+++ Sheet #2 (Loops)/Sheet#2_U.cpp
@@ -1,1 +1,27 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int sumOfDigits(int x) {
+    int sum = 0;
+    while (x > 0) {
+        sum += x % 10;
+        x /= 10;
+    }
+    return sum;
+}
+ 
+int main() {
+    int N, A, B;
+    cin >> N >> A >> B;
+ 
+    int total = 0;
+    for (int i = 1; i <= N; i++) {
+        int digitSum = sumOfDigits(i);
+        if (digitSum >= A && digitSum <= B) {
+            total += i;
+        }
+    }
+ 
+    cout << total << endl;
+    return 0;
+}
