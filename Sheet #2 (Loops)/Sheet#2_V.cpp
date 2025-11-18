afaf5aee0ed7531bb223524fd8a8623fbc18bfc4
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-18 23:36:38 +0530

    Update Sheet#2_V.cpp

--- Sheet #2 (Loops)/Sheet#2_V.cpp
+++ Sheet #2 (Loops)/Sheet#2_V.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    short int n;
+    cin>>n;
+    int j=1;
+    for(int i=1; i<=n; i++){
+        cout<<j++<<" "<<j++<<" "<<j++<<" "<<"PUM"<<endl;
+        j++;
+    }
+}
