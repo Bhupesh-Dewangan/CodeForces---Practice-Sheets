87fd1e20bf17a29b3c01f4c705ed5e2007c40948
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-18 23:37:16 +0530

    Implement pattern printing using nested loops
    Added nested loops to print a pattern based on user input.

--- Sheet #2 (Loops)/Sheet#2_W.cpp
+++ Sheet #2 (Loops)/Sheet#2_W.cpp
@@ -1,1 +1,34 @@
-
+using namespace std;
+ 
+int main() {
+    short int n;
+    cin>>n;
+    
+    for(int i=0; i<n; i++){
+        for(int j =n-1; j > i; j--)
+            cout<<" ";
+        
+        for(int j=0; j<=i; j++)
+            cout<<"*";
+        
+        for(int j=0; j<i; j++)
+            cout<<"*";
+            
+        cout<<endl;
+    }
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++)
+            cout<<" ";
+            
+        for(int j =n-1; j >= i; j--)
+            cout<<"*";
+        
+        for(int j=0; j< n-1-i; j++)
+            cout<<"*";
+    
+        cout<<endl;
+    }
+ 
+    return 0;
+}
