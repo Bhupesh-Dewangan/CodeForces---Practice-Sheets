8064a834ccbeba91f747493f7894df443d278ac3
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-19 21:20:53 +0530

    Update Sheet#2_Z.cpp

--- Sheet #2 (Loops)/Sheet#2_Z.cpp
+++ Sheet #2 (Loops)/Sheet#2_Z.cpp
@@ -1,3 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long K, S;
+    if (!(cin >> K >> S)) return 0;
+    
+    long long ans = 0;
+    for (long long x = 0; x <= K; ++x) {
+        long long L = max(0LL, S - x - K);
+        long long R = min(K,   S - x);
+        if (L <= R) ans += (R - L + 1);
+    }
+    cout << ans << '\n';
+    return 0;
+}
 
 
