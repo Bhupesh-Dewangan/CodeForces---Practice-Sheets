9fe4d5f4644fb0734028c7a9a09727d14984e525
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-19 21:20:25 +0530

    Update Sheet#2_Y.cpp

--- Sheet #2 (Loops)/Sheet#2_Y.cpp
+++ Sheet #2 (Loops)/Sheet#2_Y.cpp
@@ -1,2 +1,18 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    short int n;
+    cin>>n;
+    
+    int a=0, b=1, c=0;
+    for(int i=0; i<n; i++){
+        cout<<a<<" ";
+        c = a+b;
+        a= b;
+        b = c;
+    }
+ 
+    return 0;
+}
 
