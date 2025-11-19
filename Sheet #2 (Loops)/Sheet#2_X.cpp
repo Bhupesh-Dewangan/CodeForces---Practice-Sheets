86cd1e8a1965c3badc1b21504e0c7433bf7b0b63
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-19 21:19:42 +0530

    Update Sheet#2_X.cpp

--- Sheet #2 (Loops)/Sheet#2_X.cpp
+++ Sheet #2 (Loops)/Sheet#2_X.cpp
@@ -1,1 +1,28 @@
-1
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        unsigned long long n;
+        cin>>n;
+    
+        int count=0;
+        while(n != 0){
+            if(n&1) count++;
+            n = n>>1;
+        }
+        int base = 1;
+        unsigned long long ans = 0;
+        while(count--){
+            ans += base;
+            base *= 2;
+        }
+    
+        cout<<ans<<endl;
+    }
+    return 0;
+}
