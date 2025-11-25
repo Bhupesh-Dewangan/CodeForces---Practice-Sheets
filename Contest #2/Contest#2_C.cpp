f62a97ffe5364525901a33032bd4feb221058030
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-25 16:45:26 +0530

    Implement minimum finding in segments of array

--- Contest #2/Contest#2_C.cpp
+++ Contest #2/Contest#2_C.cpp
@@ -1,1 +1,24 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int N, K;
+    cin >> N >> K;
+ 
+    vector<long long> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+ 
+    for (int i = 0; i < N; i += K) {
+        long long mn = arr[i];
+        for (int j = i; j < min(i + K, N); j++) {
+            mn = min(mn, arr[j]);
+        }
+        cout << mn << " ";
+    }
+    cout << "\n";
+ 
+    return 0;
+}
