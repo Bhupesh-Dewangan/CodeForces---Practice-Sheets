29262405f9c25ca2ecb394e483ff060d60a66630
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-25 16:45:56 +0530

    Implement sum calculation for range queries

--- Contest #2/Contest#2_D.cpp
+++ Contest #2/Contest#2_D.cpp
@@ -1,1 +1,20 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+long long sumUpto(long long n) {
+    return n * (n + 1) / 2;
+}
+ 
+int main() {
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        long long L, R;
+        cin >> L >> R;
+        if (L > R) swap(L, R);   // ensure L <= R
+        long long ans = sumUpto(R) - sumUpto(L - 1);
+        cout << ans << "\n";
+    }
+    return 0;
+}
