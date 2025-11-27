977da1df8ea9b03e529c06c7df3a5024a98817a1
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-27 21:23:30 +0530

    Update Contest#2_E.cpp

--- Contest #2/Contest#2_E.cpp
+++ Contest #2/Contest#2_E.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    unsigned long long n;
+    cin>>n;
+    unsigned long long row = n/4;
+    cout<<n/4<<" ";
+    if(row%2 == 0){
+        cout<<n%4;
+    }
+    else{
+        cout<<(3 - n%4);
+    }
+    
+}
