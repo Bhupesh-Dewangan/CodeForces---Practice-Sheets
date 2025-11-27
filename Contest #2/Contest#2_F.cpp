b30f080ef1d080a6297d3435aa710a8314bf8d88
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-11-27 21:24:02 +0530

    Implement trailing zeros count in Contest#2_F.cpp

--- Contest #2/Contest#2_F.cpp
+++ Contest #2/Contest#2_F.cpp
@@ -1,1 +1,22 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    cin >> n;
+    long long x;
+    int maxCount = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        // count trailing zeros (faster than loop)
+        int count = __builtin_ctzll(x);  
+        maxCount = max(maxCount, count);
+    }
+ 
+    cout << maxCount << "\n";
+    return 0;
+}
