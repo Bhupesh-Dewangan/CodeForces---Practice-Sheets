9d5dbbc8ad304a59808b21726aa108934a3e299a
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-03 19:38:20 +0530

    Update Sheet#3_B.cpp

--- Sheet #3 (Arrays)/Sheet#3_B.cpp
+++ Sheet #3 (Arrays)/Sheet#3_B.cpp
@@ -1,1 +1,24 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int N;
+    cin >> N;
+    vector<long long> A(N);   // Since Ai ≤ 1e9, use long long
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    long long X;
+    cin >> X;
+ 
+    int position = -1;
+    for (int i = 0; i < N; i++) {
+        if (A[i] == X) {
+            position = i;
+            break;  // stop at first occurrence
+        }
+    }
+ 
+    cout << position << endl;
+    return 0;
+}
