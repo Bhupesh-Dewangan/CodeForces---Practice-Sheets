3cf3f807f2180091ca0bdf1289885669d0e4f922
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-03 19:37:48 +0530

    Update Sheet#3_A.cpp

--- Sheet #3 (Arrays)/Sheet#3_A.cpp
+++ Sheet #3 (Arrays)/Sheet#3_A.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin>>n;
+    long long int sum=0;
+    int temp;
+    for(int i=0; i<n; i++){
+        cin>>temp;
+        sum += temp;
+    }
+    
+    cout<<abs(sum);
+}
