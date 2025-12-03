acccac3a8c6c2b699e5f4eb0481b41b2e17af62d
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-03 19:40:04 +0530

    Update Sheet#3_E.cpp

--- Sheet #3 (Arrays)/Sheet#3_E.cpp
+++ Sheet #3 (Arrays)/Sheet#3_E.cpp
@@ -1,1 +1,19 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    int temp, low=INT_MAX, index = -1;
+    for(int i=1; i<=n; i++){
+        cin>>temp;
+        if(temp < low){
+            low = temp;
+            index = i;
+        }
+    }
+    
+    cout<<low<<" "<<index;
+ 
+    return 0;
+}
