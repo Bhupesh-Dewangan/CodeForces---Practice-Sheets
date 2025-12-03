b485e4ec3b34639bbdfb7f1817405f01f246e863
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-03 19:38:58 +0530

    Update Sheet#3_C.cpp

--- Sheet #3 (Arrays)/Sheet#3_C.cpp
+++ Sheet #3 (Arrays)/Sheet#3_C.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin>>n;
+    
+    int temp;
+    for(int i=0; i<n; i++){
+        cin>>temp;
+        if(temp == 0) cout<<"0 ";
+        else if(temp > 0) cout<<"1 ";
+        else cout<<"2 ";
+    }
+ 
+    return 0;
+}
