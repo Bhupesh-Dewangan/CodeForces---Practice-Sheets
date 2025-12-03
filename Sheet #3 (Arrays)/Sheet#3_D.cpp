7588c4bec672fabd53e182996626ad7583c2af87
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-03 19:39:29 +0530

    Implement input and output for integers in main
    Add input handling for integers and output for values <= 10.

--- Sheet #3 (Arrays)/Sheet#3_D.cpp
+++ Sheet #3 (Arrays)/Sheet#3_D.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+   int n;
+   cin>>n;
+   
+   int temp;
+   for(int i=0; i<n; i++){
+       cin>>temp;
+       if(temp <= 10) cout<<"A["<<i<<"]"<<" = "<<temp<<endl;
+   }
+ 
+    return 0;
+}
