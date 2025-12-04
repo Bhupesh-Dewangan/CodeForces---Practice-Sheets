227349964e5784217045ec304ad29c812e991b2b
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-04 19:07:57 +0530

    Implement minimum element frequency check

--- Sheet #3 (Arrays)/Sheet#3_J.cpp
+++ Sheet #3 (Arrays)/Sheet#3_J.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    // Find minimum element
+    int minElement = *min_element(A.begin(), A.end());
+
+    // Count its frequency
+    int freq = count(A.begin(), A.end(), minElement);
+
+    // Check odd/even
+    if (freq % 2 == 1)
+        cout << "Lucky" << endl;
+    else
+        cout << "Unlucky" << endl;
+
+    return 0;
+}
 
