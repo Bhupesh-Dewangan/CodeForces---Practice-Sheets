ba5cf28fd6a88f2e36a3296a8a4001f0d5dbd071
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-04 19:07:28 +0530

    Correct array input and minimum sum calculation
    Fix input reading and sum calculation logic.

--- Sheet #3 (Arrays)/Sheet#3_I.cpp
+++ Sheet #3 (Arrays)/Sheet#3_I.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    short int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        int sum =0, minSum = INT_MAX;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                sum = arr[i] + arr[j] + j - i;
+                minSum = min(minSum, sum);
+            }
+        }
+        
+        cout<<minSum<<endl;
+        
+        
+        
+    }
+
+    return 0;
+}
