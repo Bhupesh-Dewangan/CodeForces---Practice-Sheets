a3060578f4880879826913a1161ecb579895d2d0
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-05 19:47:29 +0530

    Update Sheet#3_P.cpp

--- Sheet #3 (Arrays)/Sheet#3_P.cpp
+++ Sheet #3 (Arrays)/Sheet#3_P.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<long long> A(N);
+
+    for (int i = 0; i < N; i++) cin >> A[i];
+
+    int minCount = INT_MAX;
+
+    for (int i = 0; i < N; i++) {
+        int cnt = 0;
+        while (A[i] % 2 == 0) {
+            A[i] /= 2;
+            cnt++;
+        }
+        minCount = min(minCount, cnt);
+    }
+
+    cout << minCount << endl;
+    return 0;
+}
 
