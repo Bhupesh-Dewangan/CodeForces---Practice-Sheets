d79fb44275fe499f0134046681b673f677dc385a
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-05 19:45:57 +0530

    Implement swapping of max and min array elements
    Swap the maximum and minimum elements in an array.

--- Sheet #3 (Arrays)/Sheet#3_M.cpp
+++ Sheet #3 (Arrays)/Sheet#3_M.cpp
@@ -1,1 +1,31 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    int maxIndex = -1, minIndex=-1, maxi=INT_MIN, mini=INT_MAX;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        
+        if(arr[i] > maxi){
+            maxIndex = i; 
+            maxi = arr[i];
+        }
+        
+        if(arr[i] < mini){
+            minIndex = i; 
+            mini = arr[i];
+        }
+        
+    }
+    swap(arr[minIndex], arr[maxIndex]);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
