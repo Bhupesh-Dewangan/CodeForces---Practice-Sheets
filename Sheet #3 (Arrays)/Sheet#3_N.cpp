655d9f046fcdb7a503d922c5e0a44bcbbcc11c58
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-05 19:46:19 +0530

    Implement input validation in main function
    Added input validation for string format and length.

--- Sheet #3 (Arrays)/Sheet#3_N.cpp
+++ Sheet #3 (Arrays)/Sheet#3_N.cpp
@@ -1,1 +1,31 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a,b;
+    cin>>a>>b;
+    
+    string s;
+    cin>>s;
+    
+    if(s[a] != '-'){
+        cout<<"No";
+        return 0;
+    }
+    
+    if(s.length() != a+b+1){
+        cout<<"No";
+        return 0;
+    }
+    
+    for(int i=0; i<s.length(); i++){
+        if( !isdigit(s[i]) && i != a ){
+            cout<<"No";
+            return 0;
+        }
+    }
+    
+    cout<<"Yes";
+ 
+    return 0;
+}
