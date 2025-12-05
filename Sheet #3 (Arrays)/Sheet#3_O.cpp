04406382ea845e06aa6b73480fb167d178a8bb44
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-05 19:46:54 +0530

    Update Sheet#3_O.cpp

--- Sheet #3 (Arrays)/Sheet#3_O.cpp
+++ Sheet #3 (Arrays)/Sheet#3_O.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+    if (n == 2) {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    long long a = 0, b = 1, c;
+    for (int i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    cout << b << endl;
+    return 0;
+}
 
