676c637fed9cf868e59ec9762b06c57e84d8ac4c
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-05 19:45:32 +0530

    Update Sheet#3_L.cpp

--- Sheet #3 (Arrays)/Sheet#3_L.cpp
+++ Sheet #3 (Arrays)/Sheet#3_L.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T; // number of test cases
+    while (T--) {
+        int N;
+        cin >> N; // size of array
+        vector<int> A(N);
+        for (int i = 0; i < N; i++) cin >> A[i];
+        
+        // Generate all subarray maximums
+        for (int i = 0; i < N; i++) {
+            int currentMax = A[i];
+            for (int j = i; j < N; j++) {
+                currentMax = max(currentMax, A[j]);
+                cout << currentMax << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
 
