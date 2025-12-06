a68d0d3264cb9d9645502ca6daf7e29047a51051
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-06 17:22:31 +0530

    Update Sheet#3_V.cpp

--- Sheet #3 (Arrays)/Sheet#3_V.cpp
+++ Sheet #3 (Arrays)/Sheet#3_V.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n,m;
+    cin>>n>>m;
+    
+    int temp;
+    int arr[m]={0};
+    for(int i=0; i<n; i++){
+        cin>>temp;
+        arr[temp-1]++;
+    }
+    
+    for(int i=0; i<m; i++){
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
