244ccafc62e71f2031e47419ed74c3851212e0cd
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-06 17:21:30 +0530

    Update Sheet#3_U.cpp

--- Sheet #3 (Arrays)/Sheet#3_U.cpp
+++ Sheet #3 (Arrays)/Sheet#3_U.cpp
@@ -1,1 +1,25 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+
+    vector<long long> A(N), B(M);
+    for (int i = 0; i < N; i++) cin >> A[i];
+    for (int j = 0; j < M; j++) cin >> B[j];
+
+    int i = 0, j = 0;
+    while (i < N && j < M) {
+        if (A[i] == B[j]) {
+            j++; // move in B if we find a match
+        }
+        i++; // always move in A
+    }
+
+    if (j == M) cout << "YES\n"; 
+    else cout << "NO\n";
+
+    return 0;
+}
