9e26f245a95b227d0543ff0c403267d08cf6a873
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-06 17:19:44 +0530

    Update Sheet#3_S.cpp

--- Sheet #3 (Arrays)/Sheet#3_S.cpp
+++ Sheet #3 (Arrays)/Sheet#3_S.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n,m;
+    cin>>n>>m;
+    
+    int mat[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++)
+            cin>>mat[i][j];
+    }
+    
+    int key;
+    cin>>key;
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++)
+            if(mat[i][j] == key){
+                cout<<"will not take number"<<endl;
+                return 0;
+            }
+    }
+    
+    cout<<"will take number"<<endl;
+
+    return 0;
+}
