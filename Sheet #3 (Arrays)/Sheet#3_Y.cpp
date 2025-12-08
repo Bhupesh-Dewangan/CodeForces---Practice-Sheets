56ab4369abd5d47520d5fc7777b8887db004c4be
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-08 17:05:33 +0530

    Update Sheet#3_Y.cpp

--- Sheet #3 (Arrays)/Sheet#3_Y.cpp
+++ Sheet #3 (Arrays)/Sheet#3_Y.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<long long> A(N+1), prefix(N+1, 0);
+
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+        prefix[i] = prefix[i-1] + A[i];
+    }
+
+    while (Q--) {
+        int L, R;
+        cin >> L >> R;
+        cout << prefix[R] - prefix[L-1] << "\n";
+    }
+
+    return 0;
+}
 
