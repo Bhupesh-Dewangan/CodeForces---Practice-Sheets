9f32a4e98784a0574e107bcb7438626b7fa6b445
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-08 17:04:25 +0530

    Update Sheet#3_W.cpp

--- Sheet #3 (Arrays)/Sheet#3_W.cpp
+++ Sheet #3 (Arrays)/Sheet#3_W.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n,m;
+    cin>>n>>m;
+    
+    int arr[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        int left=0, right=m-1;
+        while(left < right){
+            swap( arr[i][left], arr[i][right]);
+            left++;
+            right--;
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    
+
+    return 0;
+}
