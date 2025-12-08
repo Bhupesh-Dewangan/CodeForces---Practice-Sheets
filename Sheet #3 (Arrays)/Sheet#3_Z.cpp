26482251f7b77023395495e7c5d7cb9760cf6258
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-08 17:05:58 +0530

    Update Sheet#3_Z.cpp

--- Sheet #3 (Arrays)/Sheet#3_Z.cpp
+++ Sheet #3 (Arrays)/Sheet#3_Z.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,q;
+    cin>>n>>q;
+    
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    sort(arr, arr+n);
+    
+    int key;
+    for(int i=0; i<q; i++){
+        cin>>key;
 
+    if (binary_search(arr, arr+n, key))
+        cout <<"found\n";
+    else
+        cout <<"not found\n";
+
+    }
+    return 0;
+}
