86f66d2481de0bf7bff9eff58a76b8be5def2794
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-11 22:55:03 +0530

    Update Contest#3.1_D.cpp

--- Contest #3.1/Contest#3.1_D.cpp
+++ Contest #3.1/Contest#3.1_D.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    unordered_set<int> s(a.begin(), a.end()); // store all elements for quick lookup
+    
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (s.count(a[i] + 1)) // check if ai+1 exists
+            count++;
+    }
+    
+    cout << count;
+    return 0;
+}
 
