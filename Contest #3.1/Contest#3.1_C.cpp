f707e565bc840bd5ef089a053b159bb5544a0174
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-11 22:54:41 +0530

    Update Contest#3.1_C.cpp

--- Contest #3.1/Contest#3.1_C.cpp
+++ Contest #3.1/Contest#3.1_C.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    vector<long long> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    // Sort in descending order
+    sort(a.begin(), a.end(), greater<long long>());
+
+    long long sum = 0;
+    for(int i = 0; i < k; i++) {
+        if(a[i] > 0) sum += a[i];  // Only add positive values
+    }
+
+    cout << sum << endl;
+    return 0;
+}
 
