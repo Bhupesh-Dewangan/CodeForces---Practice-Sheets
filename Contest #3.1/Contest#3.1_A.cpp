e2f310ea8fa8dce7052122f1288fa69ed5aff6fd
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-11 22:53:53 +0530

    Update Contest#3.1_A.cpp

--- Contest #3.1/Contest#3.1_A.cpp
+++ Contest #3.1/Contest#3.1_A.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        
+        if(x == y) cout<<"Square\n";
+        else cout<<"Rectangle\n";
+    }
+
+    return 0;
+}
