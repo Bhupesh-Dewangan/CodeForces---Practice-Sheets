155799d17e819307a95013dfbadf283aec982a50
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-12 23:15:51 +0530

    Implement array input and reverse output

--- Contest #3.1/Contest#3.1_F.cpp
+++ Contest #3.1/Contest#3.1_F.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    int i=0, j=n-1;
+    while(i <= j){
+        if(i!=j){
+            cout<<arr[i++]<<" "<<arr[j--]<<" ";
+        }
+        else{
+            cout<<arr[i++];
+        }
+    }
+
+    return 0;
+}
