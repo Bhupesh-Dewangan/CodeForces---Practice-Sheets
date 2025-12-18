579796728e95f419efa998d3e7219282d47870b1
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-18 21:15:29 +0530

    Update Sheet#4_A.cpp

--- Sheet #4 (Strings)/Sheet#4_A.cpp
+++ Sheet #4 (Strings)/Sheet#4_A.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string s,t;
+    cin>>s>>t;
+    
+    cout<<s.length()<<" "<<t.length()<<endl;
+    cout<<s<<" "<<t<<endl;
+ 
+    return 0;
+}
