23f5f3ed049f2e6792260ef4c3107801c57d5289
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-18 21:16:04 +0530

    Update Sheet#4_B.cpp

--- Sheet #4 (Strings)/Sheet#4_B.cpp
+++ Sheet #4 (Strings)/Sheet#4_B.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string s;
+    getline(cin, s);
+    
+    for(char c: s){
+        if(c == '\\') break;
+        cout<<c;
+    }
+ 
+    return 0;
+}
