8d2ac1077dc82cbcc6b4716fc9943e2b6661469f
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-21 21:14:56 +0530

    Implement palindrome check in Sheet#4_I.cpp

--- Sheet #4 (Strings)/Sheet#4_I.cpp
+++ Sheet #4 (Strings)/Sheet#4_I.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin>>s;
+    
+    int low=0, high=s.length()-1;
+    while(low < high){
+        if(s[low++] != s[high--]) {
+            cout<<"NO";
+            return 0;
+        }
+    }
+    
+    cout<<"YES";
+ 
+    return 0;
+}
