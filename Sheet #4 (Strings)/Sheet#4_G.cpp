c6a8039e52ffb4439effcb21370f0534766320a5
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-21 21:14:02 +0530

    Update Sheet#4_G.cpp

--- Sheet #4 (Strings)/Sheet#4_G.cpp
+++ Sheet #4 (Strings)/Sheet#4_G.cpp
@@ -1,1 +1,15 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string s;
+    getline(cin, s);
+    
+    for(int i=0; i<s.length(); i++){
+        if(s[i]==',') cout<<" ";
+        else if( s[i] >= 'a' && s[i] <= 'z') cout<<char('A' + (s[i] - 'a'));
+        else cout<<char('a' + (s[i] - 'A'));
+    }
+ 
+    return 0;
+}
