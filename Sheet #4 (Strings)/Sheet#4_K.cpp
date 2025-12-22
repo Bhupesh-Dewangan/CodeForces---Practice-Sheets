94bba0b873921e7eec7401980ccedcec95ce037f
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-22 22:23:47 +0530

    Update Sheet#4_K.cpp

--- Sheet #4 (Strings)/Sheet#4_K.cpp
+++ Sheet #4 (Strings)/Sheet#4_K.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string s1,s2;
+        cin>>s1>>s2;
+        
+        int i=0;
+        while( i < s1.length() && i < s2.length()){
+            cout<<s1[i]<<s2[i];
+            i++;
+        }
+        
+        while( i < s1.length() ){
+            cout<<s1[i];
+            i++;
+        }
+        
+        while(i < s2.length()){
+            cout<<s2[i];
+            i++;
+        }
+        
+        cout<<endl;
+    }
+
+    return 0;
+}
