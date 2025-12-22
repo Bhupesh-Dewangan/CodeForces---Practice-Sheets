148bdae8677aaf391bcc3999c7ca32e6d7dc313a
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-22 22:23:16 +0530

    Update Sheet#4_J.cpp

--- Sheet #4 (Strings)/Sheet#4_J.cpp
+++ Sheet #4 (Strings)/Sheet#4_J.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string s;
+    cin>>s;
+    
+    int freq[26] = {0};
+    for(char c: s){
+        freq[c-'a']++;
+    }
+    
+    for(int i=0; i<26; i++){
+        if(freq[i] > 0){
+            cout<<char('a'+i)<<" : "<<freq[i]<<endl;
+        }
+    }
+
+    return 0;
+}
