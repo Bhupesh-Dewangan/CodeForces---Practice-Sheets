c8b974006dc62b3d75c748ed0eb85c0ed1626c98
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-24 19:08:44 +0530

    Add string matching for target 'hello'
    Implement string matching to check for 'hello'.

--- Sheet #4 (Strings)/Sheet#4_M.cpp
+++ Sheet #4 (Strings)/Sheet#4_M.cpp
@@ -1,3 +1,24 @@
-.
-.
-.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+
+    string target = "hello";
+    int j = 0;
+
+    for(int i = 0; i < S.size() && j < target.size(); i++) {
+        if(S[i] == target[j]) {
+            j++;
+        }
+    }
+
+    if(j == target.size()) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
+    return 0;
+}
