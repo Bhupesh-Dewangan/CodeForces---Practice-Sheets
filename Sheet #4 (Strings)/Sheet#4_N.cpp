6105f614744f337f8c93f08376da7133e4204c80
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-25 16:52:55 +0530

    Update Sheet#4_N.cpp

--- Sheet #4 (Strings)/Sheet#4_N.cpp
+++ Sheet #4 (Strings)/Sheet#4_N.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    string S;
+    cin >> N >> S;
+
+    char last_char = '\0';
+    int length = 0;
+
+    for (char c : S) {
+        if (c != last_char) {
+            length++;
+            last_char = c;
+        }
+    }
+
+    cout << length << endl;
+    return 0;
+}
 
