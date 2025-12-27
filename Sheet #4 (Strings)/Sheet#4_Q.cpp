d9dec291bf826465e350bf937f225a1c78532410
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-27 18:01:21 +0530

    Update Sheet#4_Q.cpp

--- Sheet #4 (Strings)/Sheet#4_Q.cpp
+++ Sheet #4 (Strings)/Sheet#4_Q.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    string s;
+    getline(cin,s);
+    
+    stack<char> st;
+    for(int i=0; i<s.length(); i++){
+        if(s[i] == ' '){
+            while(!st.empty()){
+                cout<<st.top();
+                st.pop();
+                
+            }
+            cout<<" ";
+        }
+        else
+        st.push(s[i]);
+    }
+    
+    while(!st.empty()){
+        cout<<st.top();
+        st.pop();
+    }
+
+    return 0;
+}
