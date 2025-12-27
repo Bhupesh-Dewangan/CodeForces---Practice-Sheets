e6264ed262db416b30c282db8173324d3d58be25
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-27 18:00:55 +0530

    Update Sheet#4_P.cpp

--- Sheet #4 (Strings)/Sheet#4_P.cpp
+++ Sheet #4 (Strings)/Sheet#4_P.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    getline(cin, S);
+
+    int count = 0;
+    bool inWord = false;
+
+    for (char c : S) {
+        if (isalpha(c)) {   // check if letter
+            if (!inWord) {
+                count++;
+                inWord = true;
+            }
+        } else {
+            inWord = false;
+        }
+    }
+
+    cout << count << endl;
+    return 0;
+}
 
