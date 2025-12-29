6023506dfccaa806cd01aed2fe9fa129e1e3b88c
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-29 19:47:06 +0530

    Update Sheet#4_T.cpp

--- Sheet #4 (Strings)/Sheet#4_T.cpp
+++ Sheet #4 (Strings)/Sheet#4_T.cpp
@@ -1,1 +1,33 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string S;
+    getline(cin, S);
+    
+    // Extract values using string operations
+    string username = S.substr(S.find("username=") + 9);
+    username = username.substr(0, username.find('&'));
+    
+    string pwd = S.substr(S.find("pwd=") + 4);
+    pwd = pwd.substr(0, pwd.find('&'));
+    
+    string profile = S.substr(S.find("profile=") + 8);
+    profile = profile.substr(0, profile.find('&'));
+    
+    string role = S.substr(S.find("role=") + 5);
+    role = role.substr(0, role.find('&'));
+    
+    string key = S.substr(S.find("key=") + 4);
+    
+    // Print results
+    cout << "username: " << username << endl;
+    cout << "pwd: " << pwd << endl;
+    cout << "profile: " << profile << endl;
+    cout << "role: " << role << endl;
+    cout << "key: " << key << endl;
+    
+    return 0;
+}
