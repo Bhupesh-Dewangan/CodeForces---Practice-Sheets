3bb5f732f75fb89c5038889fa861d2fd0db06314
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2025-12-29 19:47:46 +0530

    Implement character frequency count for 'EGYPT'
    Count the frequency of specific characters in a string and output the minimum frequency.

--- Sheet #4 (Strings)/Sheet#4_U.cpp
+++ Sheet #4 (Strings)/Sheet#4_U.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    string s;
+    cin>>s;
+    // EGYPT
+    int freq[5]={0};
+    for(char c: s){
+        if( c == 'E' || c == 'e') freq[0]++;
+        else if( c == 'G' || c == 'g') freq[1]++;
+        else if( c == 'Y' || c == 'y') freq[2]++;
+        else if( c == 'P' || c == 'p') freq[3]++;
+        else if( c == 'T' || c == 't') freq[4]++;
+        else ;
+    }
+    int ans=freq[0];
+    for(int i=0; i<5; i++){
+        ans = min(freq[i], ans);
+    }
+    
+    cout<<ans;
+
+    return 0;
+}
