4565f73615426e0899ada85709479db33c8e9c71
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-01-03 22:23:39 +0530

    Add string encryption and decryption functionality
    Implement encryption and decryption using a custom key.

--- Sheet #4 (Strings)/Sheet#4_W.cpp
+++ Sheet #4 (Strings)/Sheet#4_W.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int Q;
+    string S;
+    cin >> Q >> S;
+
+    string key = "PgEfTYaWGHjDAmxQqFLRpCJBownyUKZXkbvzIdshurMilNSVOtec#@_!=.+-*/";
+    string original = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    unordered_map<char, char> encryptMap, decryptMap;
+
+    // Build encryption and decryption mappings
+    for (int i = 0; i < (int)original.size(); i++) {
+        encryptMap[original[i]] = key[i];
+        decryptMap[key[i]] = original[i];
+    }
+
+    string result = "";
+    if (Q == 1) {
+        // Encryption
+        for (char c : S) {
+            result += encryptMap[c];
+        }
+    } else {
+        // Decryption
+        for (char c : S) {
+            result += decryptMap[c];
+        }
+    }
+
+    cout << result;
+    return 0;
+}
 
