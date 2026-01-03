4d57c6e1256ff0c590b8d11281252572c4a5893c
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-01-03 22:22:23 +0530

    Update Sheet#4_V.cpp

--- Sheet #4 (Strings)/Sheet#4_V.cpp
+++ Sheet #4 (Strings)/Sheet#4_V.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() {
+    string s;
+    getline(cin, s);
+
+    size_t pos = 0;
+    while ((pos = s.find("EGYPT", pos)) != string::npos) {
+        s.replace(pos, 5, " ");
+        pos += 1; // Move forward to avoid infinite loop on overlapping matches
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
