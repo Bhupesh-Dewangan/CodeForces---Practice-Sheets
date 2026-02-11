f316867e39da8329a5f12539ddfc8aa8150c7885
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-11 16:56:05 +0530

    Update Sheet#5_D.cpp

--- Sheet #5 (Functions)/Sheet#5_D.cpp
+++ Sheet #5 (Functions)/Sheet#5_D.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+bool checkPrime(int n){
+    if(n == 1) return false;
+    
+    for(int i=2; i*i <= n; i++){
+        if(n%i == 0) return false;
+    }
+    
+    return true;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin>>n;
+        
+        if(checkPrime(n)) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+
+    return 0;
+}
