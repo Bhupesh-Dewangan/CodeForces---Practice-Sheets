e998c8106f6cf54afbc352718bdac6b0288f7da0
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-12 22:25:51 +0530

    Update Sheet#5_E.cpp

--- Sheet #5 (Functions)/Sheet#5_E.cpp
+++ Sheet #5 (Functions)/Sheet#5_E.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+void exchange(int &x, int &y){
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+    exchange(x,y);
+    cout<<x<<" "<<y;
+
+    return 0;
+}
