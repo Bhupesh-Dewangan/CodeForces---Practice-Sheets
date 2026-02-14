e5b72aa724539fa8f4ac64d6e39d3888d7ef6357
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-14 22:31:15 +0530

    Update Sheet#5_G.cpp

--- Sheet #5 (Functions)/Sheet#5_G.cpp
+++ Sheet #5 (Functions)/Sheet#5_G.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void findMinMax(int arr[], int n){
+    int min=INT_MAX, max=INT_MIN;
+    for(int i=0; i<n; i++){
+        if(arr[i] < min) min = arr[i];
+        if(arr[i] > max) max = arr[i];
+    }
+    
+    cout<<min<<" "<<max;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    findMinMax(arr,n);
+
+    return 0;
+}
