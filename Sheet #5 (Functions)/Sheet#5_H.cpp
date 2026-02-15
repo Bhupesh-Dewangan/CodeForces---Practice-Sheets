6085e45e640c034c11798d099b036387a66cb025
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-15 21:32:13 +0530

    Update Sheet#5_H.cpp

--- Sheet #5 (Functions)/Sheet#5_H.cpp
+++ Sheet #5 (Functions)/Sheet#5_H.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+void printNChar(int n, char c){
+    
+    for(int i=0; i<n; i++) cout<<c<<" ";
+}
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        char c;
+        cin>>n>>c;
+        
+        printNChar(n,c);
+        cout<<endl;
+    }
+
+    return 0;
+}
