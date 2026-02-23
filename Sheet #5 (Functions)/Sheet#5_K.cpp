10083f5dc7a123cda4b6abe580f54f48acc7bd5b
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-23 21:04:21 +0530

    Add right shift function for array elements
    Implement a function to shift elements of an array to the right by x positions.

--- Sheet #5 (Functions)/Sheet#5_K.cpp
+++ Sheet #5 (Functions)/Sheet#5_K.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void shiftRight(vector<int> &arr, int n, int x){
+    reverse(arr.begin(), arr.end()-x);
+    reverse(arr.end()-x, arr.end());
+    reverse(arr.begin(), arr.end());
+}
+
+int main() {
+    int n, x;
+    cin>>n>>x;
+    
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) cin>>arr[i];
+    
+    x = x%n;
+    
+    shiftRight(arr,n,x);
+    
+    for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+    
+
+    return 0;
+}
