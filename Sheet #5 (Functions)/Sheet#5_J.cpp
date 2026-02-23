e14a82c6b67216b862dcfe4bca4bab37551edd3a
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-23 21:03:56 +0530

    Update Sheet#5_J.cpp

--- Sheet #5 (Functions)/Sheet#5_J.cpp
+++ Sheet #5 (Functions)/Sheet#5_J.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+double findAvg(double arr[], int n){
+    double sum = 0;
+    for(int i=0; i<n; i++) sum += arr[i];
+    
+    return sum/n;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    
+    double arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    double ans = findAvg(arr, n);
+    cout<<fixed<<setprecision(6)<<ans;
+
+    return 0;
+}
