08689387193b8b6fa47968d2aebcea5291476393
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-24 23:18:54 +0530

    Implement CountUnique function to count unique elements

--- Sheet #5 (Functions)/Sheet#5_M.cpp
+++ Sheet #5 (Functions)/Sheet#5_M.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int CountUnique(vector<int> arr, int n){
+    unordered_map<int, int> mpp;
+    for(int i=0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+    
+    return mpp.size();
+}
+
+int main() {
+    int n;
+    cin>>n;
+    
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) 
+        cin>>arr[i];
+        
+    int ans = CountUnique(arr, n);
+    cout<<ans;
+    
+
+    return 0;
+}
