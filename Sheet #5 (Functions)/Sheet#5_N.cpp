cdbf88bae1fbb983acb209b4d3214996add3590d
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-02-26 20:48:00 +0530

    Add function to shift zeros in an array
    Implemented a function to shift all zeros in an array to the end while maintaining the order of non-zero elements.

--- Sheet #5 (Functions)/Sheet#5_N.cpp
+++ Sheet #5 (Functions)/Sheet#5_N.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void shiftZeros(vector<int> &arr, int n){
+    int j=0;
+    for(int i=0; i<n; i++){
+        if(arr[i] != 0) {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+
+    }
+}
+
+int main() {
+    int n;
+    cin>>n;
+    
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    
+    shiftZeros(arr, n);
+    
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
