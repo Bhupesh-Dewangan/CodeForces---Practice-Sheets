368de4efcc6ccf4c075f0a3bc375e989e8f9cdbf
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-03-27 22:40:17 +0530

    Update Sheet#6_A.cpp

--- Sheet #6 (Math - Geometry)/Sheet#6_A.cpp
+++ Sheet #6 (Math - Geometry)/Sheet#6_A.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(long long n) {
+    return (n > 0) && ((n & (n - 1)) == 0);
+}
+
+int main() {
+    long long num;
+    cin >> num;
+
+    if (isPowerOfTwo(num)) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+
+    return 0;
+}
 
