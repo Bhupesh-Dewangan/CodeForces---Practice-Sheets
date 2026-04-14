3e8c54224da4d7cf411794cbf3cf6cae1a93c628
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-04-14 22:40:40 +0530

    Update Sheet#6_G.cpp

--- Sheet #6 (Math - Geometry)/Sheet#6_G.cpp
+++ Sheet #6 (Math - Geometry)/Sheet#6_G.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long N;
+    cin >> N;
+
+    long long sum = 0;
+
+    for (long long i = 1; i * i <= N; i++) {
+        if (N % i == 0) {
+            sum += i;              // first divisor
+            if (i != N / i) {
+                sum += N / i;      // paired divisor
+            }
+        }
+    }
+
+    cout << sum << endl;
+    return 0;
+}
 
