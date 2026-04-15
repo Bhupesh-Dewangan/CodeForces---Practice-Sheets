3e6f5ac715daaf7d975e630f4f42166b053999a3
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-04-15 22:12:36 +0530

    Update Sheet#6_H.cpp

--- Sheet #6 (Math - Geometry)/Sheet#6_H.cpp
+++ Sheet #6 (Math - Geometry)/Sheet#6_H.cpp
@@ -1,3 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x, y;
+    cin >> x >> y;
+
+    long long gcd = __gcd(x, y);
+    long long lcm = (x / gcd) * y;
+
+    cout << gcd << " " << lcm << endl;
+
+    return 0;
+}
 
 
 
