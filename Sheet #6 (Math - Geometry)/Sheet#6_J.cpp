1cb647581e249b703706828eba3db19640ce809c
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-05-03 19:13:30 +0530

    Update Sheet#6_J.cpp

--- Sheet #6 (Math - Geometry)/Sheet#6_J.cpp
+++ Sheet #6 (Math - Geometry)/Sheet#6_J.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    long long N;
+    cin >> N;
+
+    bool first = true;
+
+    for (long long i = 2; i * i <= N; i++) {
+        if (N % i == 0) {
+            int count = 0;
+            while (N % i == 0) {
+                N /= i;
+                count++;
+            }
+            if (!first) cout << "*";
+            cout << "(" << i << "^" << count << ")";
+            first = false;
+        }
+    }
+
+    // If N is still greater than 1, it is a prime factor
+    if (N > 1) {
+        if (!first) cout << "*";
+        cout << "(" << N << "^1)";
+    }
+
+    return 0;
+}
