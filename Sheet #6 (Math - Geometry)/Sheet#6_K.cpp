ac2b84a9c205bc6713219f82e92c9f3b2db96519
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-05-04 22:15:40 +0530

    Update Sheet#6_K.cpp

--- Sheet #6 (Math - Geometry)/Sheet#6_K.cpp
+++ Sheet #6 (Math - Geometry)/Sheet#6_K.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+long long factorial(int n) {
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    long long nCr = factorial(A) / (factorial(B) * factorial(A - B));
+    long long nPr = factorial(A) / factorial(A - B);
+
+    cout << nCr << " " << nPr << endl;
+
+    return 0;
+}
