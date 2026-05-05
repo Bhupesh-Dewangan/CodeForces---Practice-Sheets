e694525d38235779eb02c6b7124589c268831d71
Bhupesh Dewangan <bhupeshdewangan160204@gmail.com>
2026-05-05 22:40:18 +0530

    Add Pascal's triangle calculation in Sheet#6_L.cpp
    Implement Pascal's triangle generation and output.

--- Sheet #6 (Math - Geometry)/Sheet#6_L.cpp
+++ Sheet #6 (Math - Geometry)/Sheet#6_L.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    int N;
+    cin >> N;
+
+    vector<vector<int>> triangle(N);
+
+    for (int i = 0; i < N; i++) {
+        triangle[i].resize(i + 1);
+        triangle[i][0] = triangle[i][i] = 1;
+
+        for (int j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << triangle[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
